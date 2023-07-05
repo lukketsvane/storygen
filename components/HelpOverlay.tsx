6c347fc6eea255f781d5fb09c8d10806b26f6787
lukketsvane <iverfinne@gmail.com>
2023-07-05 08:16:08 +0200

    remove noise

--- components/HelpOverlay.tsx
+++ components/HelpOverlay.tsx
@@ -1,27 +1,147 @@
-import { Box, Button, Flex, IconButton, useDisclosure, Heading, Text, Image, Link, AspectRatio, useBreakpointValue, UnorderedList, ListItem, HStack, Divider, Tooltip } from "@chakra-ui/react";
-import { QuestionIcon, CloseIcon, ArrowLeftIcon } from "@chakra-ui/icons";
+import React from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  IconButton,
+  useDisclosure,
+  Heading,
+  Text,
+  Image,
+  Link,
+  AspectRatio,
+  useBreakpointValue,
+  UnorderedList,
+  ListItem,
+  HStack,
+  Divider,
+  Tooltip,
+} from "@chakra-ui/react";
+import {
+  QuestionIcon,
+  CloseIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+} from "@chakra-ui/icons";
 import { useTranslation } from "next-i18next";
 import Counter from "./shared/Counter";
-import { useRouter } from "next/router";
-import ReactMarkdown from "react-markdown";
-import remarkGfm from "remark-gfm";
+import Page1 from "./pages/Page1";
+import Page2 from "./pages/Page2";
 
 const HelpOverlay = () => {
   const { isOpen, onToggle, onClose } = useDisclosure();
   const { t } = useTranslation("common");
   const isMobile = useBreakpointValue({ base: true, md: false });
-  const router = useRouter();
+  const [currentPage, setCurrentPage] = React.useState("main");
 
   const handleSalesTermsClick = () => {
     onToggle();
   };
 
   const handleArrowLeftClick = () => {
-    const { asPath, query } = router;
-    const currentPathArray = asPath.split("/");
-    currentPathArray.pop();
-    const previousPath = currentPathArray.join("/");
-    router.push({ pathname: previousPath, query });
+    setCurrentPage((prevPage) => {
+      if (prevPage === "main") {
+        return "page2";
+      } else if (prevPage === "page2") {
+        return "page1";
+      } else if (prevPage === "page1") {
+        return "main";
+      }
+    });
+  };
+
+  const handleArrowRightClick = () => {
+    setCurrentPage((prevPage) => {
+      if (prevPage === "main") {
+        return "page1";
+      } else if (prevPage === "page1") {
+        return "page2";
+      } else if (prevPage === "page2") {
+        return "main";
+      }
+    });
+  };
+
+  const renderPageContent = () => {
+    switch (currentPage) {
+      case "main":
+        return (
+          <>
+            <Heading size="xl" mt={6} mb={4}>
+              {t("storyGenTitle")}
+            </Heading>
+            <Text>{t("storyGenDescription")}</Text>
+            <Heading size="lg" mt={6} mb={2}>
+              {t("howItWorksTitle")}
+            </Heading>
+            <UnorderedList pl={4} mb={4}>
+              <ListItem>{t("step1")}</ListItem>
+              <ListItem>{t("step2")}</ListItem>
+              <ListItem>{t("step3")}</ListItem>
+            </UnorderedList>
+            <Heading size="lg" mt={6} mb={2}>
+              {t("futurePlansTitle")}
+            </Heading>
+            <Text>{t("futurePlansDescription")}</Text>
+            <Heading size="lg" mt={6} mb={2}>
+              {t("supportingCauseTitle")}
+            </Heading>
+            <Text>{t("supportingCauseDescription")}</Text>
+            <Box w="full" boxShadow="" rounded="md" overflow="hidden" mt={4}>
+              <AspectRatio ratio={16 / 6}>
+                <Link href="https://www.spleis.no/project/324720" isExternal>
+                  <Image
+                    src="https://spleisprod.s3.amazonaws.com/uploads/projects/324720/324720-79f270de-91ee-4f5a-bcd6-ae0aec6cca6c-1800.webp"
+                    alt="Support Dr. Bayan"
+                    objectFit="cover"
+                  />
+                </Link>
+              </AspectRatio>
+              <Box p={4} bg="gray.100" userSelect="none">
+                <Text fontWeight="bold">{t("supportingCauseText")}</Text>
+              </Box>
+            </Box>
+            <Text mt={4} mb={2}>
+              {t("generatedCountText")} <Counter /> {t("supportDevelopmentText")}
+            </Text>
+            <Divider />
+            <HStack spacing={12} pt={6} pb={6} justifyContent="center">
+              <Link href="https://github.com/lukketsvane" isExternal>
+                {t("githubLinkText")}
+              </Link>
+              <Text>{t("orgNumberText")}</Text>
+              <Text>{t("emailText")}</Text>
+              <Button variant="link" onClick={handleSalesTermsClick}>
+                {t("salesTermsText")}
+              </Button>
+            </HStack>
+            <HStack spacing={12} pt={6} pb={6} justifyContent="center">
+              <IconButton
+                colorScheme="teal"
+                aria-label="Previous"
+                icon={<ArrowLeftIcon />}
+                onClick={handleArrowLeftClick}
+                borderRadius="50%"
+                disabled={!isOpen}
+              />
+              <IconButton
+                colorScheme="teal"
+                aria-label="Next"
+                icon={<ArrowRightIcon />}
+                onClick={handleArrowRightClick}
+                borderRadius="50%"
+                disabled={!isOpen}
+              />
+            </HStack>
+          </>
+        );
+      case "page1":
+        return <Page1 />;
+      case "page2":
+        return <Page2 />;
+      default:
+        return null;
+    }
   };
 
   return (
@@ -42,6 +162,20 @@ const HelpOverlay = () => {
           onClick={onToggle}
           borderRadius="50%"
         />
+        {isOpen && (
+          <Tooltip label="Back" placement="left" openDelay={300}>
+            <IconButton
+              colorScheme="teal"
+              aria-label="Back"
+              fontSize="20px"
+              icon={<ArrowLeftIcon />}
+              onClick={handleArrowLeftClick}
+              borderRadius="50%"
+              mt={2}
+              disabled={currentPage === "main"}
+            />
+          </Tooltip>
+        )}
       </Flex>
 
       {isOpen && (
@@ -66,72 +200,7 @@ const HelpOverlay = () => {
             w={isMobile ? "100vw" : "60%"}
             mx={isMobile ? 0 : "auto"}
           >
-            <Flex align="center">
-              {router.pathname !== "/docs" && (
-                <Tooltip label={t("back")} hasArrow>
-                  <IconButton
-                    icon={<ArrowLeftIcon />}
-                    aria-label={t("back")}
-                    onClick={handleArrowLeftClick}
-                    mr={2}
-                  />
-                </Tooltip>
-              )}
-              <Heading size="xl" mt={6} mb={4}>
-                {t("storyGenTitle")}
-              </Heading>
-            </Flex>
-
-            <Text>{t("storyGenDescription")}</Text>
-
-            <Heading size="lg" mt={6} mb={2}>
-              {t("howItWorksTitle")}
-            </Heading>
-            <UnorderedList pl={4} mb={4}>
-              <ListItem>{t("step1")}</ListItem>
-              <ListItem>{t("step2")}</ListItem>
-              <ListItem>{t("step3")}</ListItem>
-            </UnorderedList>
-
-            <Heading size="lg" mt={6} mb={2}>
-              {t("futurePlansTitle")}
-            </Heading>
-            <Text>{t("futurePlansDescription")}</Text>
-
-            <Heading size="lg" mt={6} mb={2}>
-              {t("supportingCauseTitle")}
-            </Heading>
-            <Text>{t("supportingCauseDescription")}</Text>
-
-            <Box w="full" boxShadow="" rounded="md" overflow="hidden" mt={4}>
-              <AspectRatio ratio={16 / 6}>
-                <Link href="https://www.spleis.no/project/324720" isExternal>
-                  <Image
-                    src="https://spleisprod.s3.amazonaws.com/uploads/projects/324720/324720-79f270de-91ee-4f5a-bcd6-ae0aec6cca6c-1800.webp"
-                    alt="Support Dr. Bayan"
-                    objectFit="cover"
-                  />
-                </Link>
-              </AspectRatio>
-              <Box p={4} bg="gray.100" userSelect="none">
-                <Text fontWeight="bold">{t("supportingCauseText")}</Text>
-              </Box>
-            </Box>
-
-            <Text mt={4} mb={2}>
-              {t("generatedCountText")} <Counter /> {t("supportDevelopmentText")}
-            </Text>
-            <Divider />
-            <HStack spacing={12} pt={6} pb={6} justifyContent="center">
-              <Link href="https://github.com/lukketsvane" isExternal>
-                {t("githubLinkText")}
-              </Link>
-              <Text>{t("orgNumberText")}</Text>
-              <Text>{t("emailText")}</Text>
-              <Button variant="link" onClick={handleSalesTermsClick}>
-                {t("salesTermsText")}
-              </Button>
-            </HStack>
+            {renderPageContent()}
           </Box>
         </Box>
       )}
---
+++ components/pages/Page2.tsx
@@ -0,0 +1,12 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+const Page1 = () => {
+  return (
+    <Box>
+      <Heading size="lg" mb={4}>Page 2</Heading>
+      <Text>This is Page 2 content.</Text>
+    </Box>
+  );
+};
+
+export default Page1;
